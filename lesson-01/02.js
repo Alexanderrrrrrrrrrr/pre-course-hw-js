5fa0b4ec33146ddde6c74fe2f5a4ff742facc6e2
Alexander <z4t@yandex.ru>
2023-11-07 11:44:11 +0300

    четное не четное число

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
 // code
+let number = Math.floor(Math.random() * 100)
+if (number % 2 === 0) {
+    console.log("Число " + number + " четное.");
+  } else {
+    console.log("Число " + number + " нечетное.");
+  }
