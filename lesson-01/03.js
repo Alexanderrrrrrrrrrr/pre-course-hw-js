c54667e4517c50005cbbc0cf04d7a8723033164c
Alexander <z4t@yandex.ru>
2023-11-07 12:49:55 +0300

    испрвление 3 задания

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -2,7 +2,9 @@
 let number = 1;
 console.log(number);
 number++;
-console.log(number + 1);
-console.log(number + 1);
+number++;
+console.log(number );
+console.log(number );
+--number;
 --number;
 console.log(number);
