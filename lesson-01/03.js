ccbcfa3fd877b07cab34460bf4ba9aae052035f6
Alexander <z4t@yandex.ru>
2023-11-07 12:38:30 +0300

    1 урок 3 задание

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -2,7 +2,7 @@
 let number = 1;
 console.log(number);
 number++;
-console.log(number ++);
-console.log(number ++);
+console.log(number + 1);
+console.log(number + 1);
 --number;
 console.log(number);
