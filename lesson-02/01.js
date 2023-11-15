3ce4e6611ae1479e22312e6dfdb08cd92da48dcd
Alexander <z4t@yandex.ru>
2023-11-15 12:18:55 +0300

    leson2task1 corect

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,8 +1,8 @@
 // code
-function doArrow(value) {
-    console.log(value);
-}
-doArrow('Передай 5')
+// function doArrow(value) {
+//     console.log(value);
+// }
+// doArrow('Передай 5')
 
 const doArrow =  (value) => {
     console.log(value);
