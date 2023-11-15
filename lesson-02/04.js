30b9a004ab18966bffd0acd338676533552b021f
Alexander <z4t@yandex.ru>
2023-11-15 15:29:10 +0300

    leson2task4

---
+++ lesson-02/04.js
@@ -0,0 +1,9 @@
+// code
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+let newPasspotr = {...passport,name: "Ivan" }
+
+console.log(passport.name); 
+console.log(newPassport.name); 
