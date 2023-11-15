af83e9faba86bb72d63f5e923f3f64c917107ac5
Alexander <z4t@yandex.ru>
2023-11-15 17:26:56 +0300

    leson 2 task7

---
+++ lesson-02/07.js
@@ -0,0 +1,14 @@
+// code
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let passportMarried2 = { ...passportMarried, married: true, } 
+ 
+console.log(passportMarried); 
+console.log(passportMarried2);
