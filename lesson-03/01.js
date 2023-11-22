eaa03e8dcb2736711be58f8de79f15500399b75c
Alexander <z4t@yandex.ru>
2023-11-22 17:33:52 +0300

    leson3 task1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -2,9 +2,8 @@
 ​const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter((color) => {
-        return color === "черный" || color === "красный" || color === "желтый";
-    }).join('-');
+    const germanFlagColors = colors.filter(color => color === "черный" || color === "красный" || color === "желтый");
+    return germanFlagColors.join('-');
 }
 
 console.log(createColorString());
